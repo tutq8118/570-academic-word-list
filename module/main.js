354b92aa03f45dd9e5ea5334af47f5e820c654bb
Quang Tu Tran <tutq8118@gmail.com>
2021-02-27 00:22:14 +0700

    finish

--- module/main.js
+++ module/main.js
@@ -1,5 +1,9 @@
-import { ACADEMIC_WORDLIST_ENDPOINT } from './datafetching.js';
-import { confirmShow } from './confirm.js';
+import {
+  ACADEMIC_WORDLIST_ENDPOINT
+} from './datafetching.js';
+import {
+  confirmShow
+} from './confirm.js';
 
 const main = async () => {
   let wordlist;
@@ -15,13 +19,13 @@ const main = async () => {
     localStorage.ACADEMIC_WORDLIST = JSON.stringify(wordlist);
   }
   let fullWordlist = JSON.parse(JSON.stringify(wordlist));
-  const settings = localStorage.ACADEMIC_WORDLIST_SETTINGS
-    ? JSON.parse(localStorage.ACADEMIC_WORDLIST_SETTINGS)
-    : {
-        color: '#2dbe60',
-        sublist: '0',
-        showCheckbox: false,
-      };
+  const settings = localStorage.ACADEMIC_WORDLIST_SETTINGS ?
+    JSON.parse(localStorage.ACADEMIC_WORDLIST_SETTINGS) :
+    {
+      color: '#2dbe60',
+      sublist: '0',
+      showCheckbox: false,
+    };
   const sublistSelected = parseInt(settings.sublist) > 0 ? parseInt(settings.sublist) : 0;
   const colorSelected = settings.color ? settings.color : '#2dbe60';
   const showCheckbox = settings.showCheckbox;
@@ -54,7 +58,12 @@ const main = async () => {
     style.appendChild(document.createTextNode(css));
   };
 
-  let { word, phonetics, meanings, sublist } = showCheckbox ? wordlist.filter((e) => e.disabled !== true)[randomize()] : wordlist[randomize()];
+  let {
+    word,
+    phonetics,
+    meanings,
+    sublist
+  } = showCheckbox ? wordlist.filter((e) => e.disabled !== true)[randomize()] : wordlist[randomize()];
 
   const renderData = (word, phonetics, meanings, sublist) => {
     const phoneticsHTML = phonetics
@@ -77,9 +86,9 @@ const main = async () => {
         <h3 class="academic__meanings-pos"><strong>Part of speech: </strong><span>${e.partOfSpeech}</span></h3>
         ${e.definitions
           .map(
-            (d) => `<p class="academic__meanings-definition" style="color: ${colorSelected}"><strong>Definition: </strong><span>${d.definition}</span></p>
-            ${d.example ? `<p class="academic__meanings-example"><strong>Example: </strong><span>${d.example}</span></p>` : ''}
-        ${d.synonyms ? `<p class="academic__meanings-syn"><strong>Synonyms: </strong><span>${d.synonyms.map((s) => `<i>${s}</i>`).join(', ')}</span></p>` : ''}`
+            (d) => `<p class="academic__meanings-definition" style="color: ${colorSelected}"><strong><u>Definition</u>:</strong> <span>${d.definition}</span></p>
+            ${d.example ? `<p class="academic__meanings-example"><strong><u>Example</u>:</strong> <span>${d.example.split(" ").map(w => (w.includes(word) ? `<strong><i>${w}</i></strong>` : w )).join(" ")}</span></p>` : ''}
+        ${d.synonyms ? `<p class="academic__meanings-syn"><strong><u>Synonyms</u>:</strong> <span>${d.synonyms.map((s) => `<i>${s}</i>`).join(', ')}</span></p>` : ''}`
           )
           .join("<hr style='border-top: #eaeaea; width: 150px; margin: 3rem auto;'>")}
       </div>
@@ -111,7 +120,7 @@ const main = async () => {
 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="recycle" class="svg-inline--fa fa-recycle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M184.561 261.903c3.232 13.997-12.123 24.635-24.068 17.168l-40.736-25.455-50.867 81.402C55.606 356.273 70.96 384 96.012 384H148c6.627 0 12 5.373 12 12v40c0 6.627-5.373 12-12 12H96.115c-75.334 0-121.302-83.048-81.408-146.88l50.822-81.388-40.725-25.448c-12.081-7.547-8.966-25.961 4.879-29.158l110.237-25.45c8.611-1.988 17.201 3.381 19.189 11.99l25.452 110.237zm98.561-182.915l41.289 66.076-40.74 25.457c-12.051 7.528-9 25.953 4.879 29.158l110.237 25.45c8.672 1.999 17.215-3.438 19.189-11.99l25.45-110.237c3.197-13.844-11.99-24.719-24.068-17.168l-40.687 25.424-41.263-66.082c-37.521-60.033-125.209-60.171-162.816 0l-17.963 28.766c-3.51 5.62-1.8 13.021 3.82 16.533l33.919 21.195c5.62 3.512 13.024 1.803 16.536-3.817l17.961-28.743c12.712-20.341 41.973-19.676 54.257-.022zM497.288 301.12l-27.515-44.065c-3.511-5.623-10.916-7.334-16.538-3.821l-33.861 21.159c-5.62 3.512-7.33 10.915-3.818 16.536l27.564 44.112c13.257 21.211-2.057 48.96-27.136 48.96H320V336.02c0-14.213-17.242-21.383-27.313-11.313l-80 79.981c-6.249 6.248-6.249 16.379 0 22.627l80 79.989C302.689 517.308 320 510.3 320 495.989V448h95.88c75.274 0 121.335-82.997 81.408-146.88z"></path></svg>
             <span class="tooltiptext">Uncheck all the words</span>
           </button>
-          <label class='academic__sublist-switch tooltip'><strong>Word Filter: </strong><input type="checkbox" id='checkbox-switch' ${settings.showCheckbox ? 'checked' : ''} /><i class="switch"></i><span class="tooltiptext">If turn on this, the words checked will no longer be shown randomly</span></label>
+          <label class='academic__sublist-switch tooltip'><strong>Word Filter: </strong><input type="checkbox" id='checkbox-switch' ${settings.showCheckbox ? 'checked' : ''} /><i class="switch"></i><span class="tooltiptext">If turn on this, the checked words will no longer be shown randomly</span></label>
         </header>
         <ul style="border-color: ${colorSelected}">
           ${sublistHTML}
